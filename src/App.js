661214b474f84587c91e7f34c69a3e61813c197b
Tino Muzambi <tinomuzambi@gmail.com>
2021-05-17 12:20:56 +0200

    Adds useEffect for api call.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,20 @@
+import { useEffect } from "react";
+
 const App = () => {
+	useEffect(() => {
+		const getData = async () => {
+			try {
+				const data = await fetch(
+					`https://gateway.marvel.com/v1/characters?apikey=${process.env.REACT_APP_MARVEL_KEY}`
+				);
+				console.log(data);
+			} catch (error) {
+				console.error(error);
+			}
+		};
+		getData();
+	}, []);
+
 	return "Clean React Boilerplate";
 };
 
