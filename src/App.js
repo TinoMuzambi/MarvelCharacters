ddec8951610cc4f9e6159b739731a66e3c19773d
Tino Muzambi <tinomuzambi@gmail.com>
2021-05-17 12:43:04 +0200

    Adds md5 for hashing.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import { useEffect } from "react";
+import md5 from "md5";
 
 const App = () => {
 	useEffect(() => {
 		const getData = async () => {
 			try {
+				const ts = 1;
+				const key = process.env.REACT_APP_MARVEL_KEY;
+				const hash = md5(
+					ts +
+						process.env.REACT_APP_MARVEL_KEY +
+						process.env.REACT_APP_MARVEL_PUBLIC_KEY
+				);
 				const data = await fetch(
-					`https://gateway.marvel.com/v1/characters?apikey=${process.env.REACT_APP_MARVEL_KEY}`
+					`https://gateway.marvel.com/v1/characters?ts=${ts}&apikey=${key}&hash=${hash}`
 				);
 				console.log(data);
 			} catch (error) {
